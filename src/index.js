d2d655c3b082e31423b349ac28b82a4d8d3701b1
David Gutiérrez <david.gutierrez@estudiants.urv.cat>
2020-12-07 12:00:02 +0100

    Agro management 0.1

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
+require('electron-reload')(__dirname, {});
+
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
   app.quit();
@@ -11,13 +13,15 @@ const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    icon: __dirname + '/images/leaf.jpg',
   });
-
+  mainWindow.setMenuBarVisibility(false);
+  mainWindow.maximize();
   // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  //mainWindow.webContents.openDevTools();
 };
 
 // This method will be called when Electron has finished
