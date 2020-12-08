f79f9e0af0e5d6f9d414ca0268c6dc1e9798a8a0
David Gutiérrez <david.gutierrez@estudiants.urv.cat>
2020-12-08 23:21:44 +0100

    últim commit

--- src/js/login.js
+++ src/js/login.js
@@ -1,8 +1,7 @@
 function onClickLogIn() {
-    if (document.getElementById("InputEmail1").value == "a@a.com") {
+    if (document.getElementById("InputEmail1").value == "admin@admin.com") {
         sessionStorage.setItem('key', 'value');
-        document.getElementById("InputEmail1").value = "hola";
-        
+        document.getElementById("InputEmail1").value = "admin";
         window.location.replace("index.html");
     }
 }
